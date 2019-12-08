a92ab650a53dc14e32a32112d85a902b10a52ca3
hirsiAbdollahi <abdollahi.hirsi@gmail.com>
2019-12-08 17:41:09 +0100

    Ajout des User, des Userfixtures, du SecurityController et du LoginFormAuthenticator

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,48 @@
+<?php
+
+
+namespace App\DataFixtures;
+
+
+use App\Entity\User;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class UserFixtures extends BaseFixture
+{
+    /** @var UserPasswordEncoderInterface */
+    private $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
+    protected function loadData(ObjectManager $manager)
+    {
+        $this->createMany(10, 'user_admin', function ($num) {
+            $admin = new User();
+            $admin
+                ->setEmail(sprintf('admin%d@kritik.fr', $num))
+                ->setRoles(['ROLE_ADMIN'])
+                ->setPassword($this->encoder->encodePassword($admin, 'admin' . $num))
+                ->setPseudo('admin_pseudo'.$num)
+            ;
+
+            return $admin;
+        });
+
+        $this->createMany(40, 'user_user', function ($num) {
+            $user = new User();
+            $user
+                ->setEmail(sprintf('user%d@mail.org', $num))
+                ->setPassword($this->encoder->encodePassword($user, 'user' . $num))
+                ->setPseudo('user_pseudo'.$num)
+            ;
+
+            return $user;
+        });
+
+        $manager->flush();
+    }
+}
---
+++ src/Migrations/Version20191206203508.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20191206203508 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE artist (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, description LONGTEXT DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE label (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE record (id INT AUTO_INCREMENT NOT NULL, artist_id INT NOT NULL, label_id INT DEFAULT NULL, title VARCHAR(255) NOT NULL, description LONGTEXT DEFAULT NULL, released_at DATE NOT NULL, INDEX IDX_9B349F91B7970CF8 (artist_id), INDEX IDX_9B349F9133B92F39 (label_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, pseudo VARCHAR(25) NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), UNIQUE INDEX UNIQ_8D93D64986CC499D (pseudo), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE record ADD CONSTRAINT FK_9B349F91B7970CF8 FOREIGN KEY (artist_id) REFERENCES artist (id)');
+        $this->addSql('ALTER TABLE record ADD CONSTRAINT FK_9B349F9133B92F39 FOREIGN KEY (label_id) REFERENCES label (id)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE record DROP FOREIGN KEY FK_9B349F91B7970CF8');
+        $this->addSql('ALTER TABLE record DROP FOREIGN KEY FK_9B349F9133B92F39');
+        $this->addSql('DROP TABLE artist');
+        $this->addSql('DROP TABLE label');
+        $this->addSql('DROP TABLE record');
+        $this->addSql('DROP TABLE user');
+    }
+}
