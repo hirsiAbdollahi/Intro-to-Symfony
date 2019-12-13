7d028e938b4e2eab4215b208706119ba3f7d8a2f
hirsiAbdollahi <abdollahi.hirsi@gmail.com>
2019-12-13 11:49:37 +0100

    Ajout de l'inscription au site

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -4,8 +4,13 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use App\Form\UserRegistrationFormType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use App\Entity\User;
 
 class SecurityController extends AbstractController
 {
@@ -26,6 +31,46 @@ class SecurityController extends AbstractController
         return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
     }
 
+
+
+     /**
+     * @Route("/register", name="app_register")
+     */
+    public function Register(Request $request, EntityManagerInterface $em, UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $user = new User();
+        $registerForm= $this->createForm(UserRegistrationFormType::class, $user);
+        $registerForm->handleRequest($request);
+
+        if ($registerForm->isSubmitted() && $registerForm->isValid()) {
+
+            $user->setEmail($registerForm['email']->getData())
+                 ->setPseudo($registerForm['pseudo']->getData())
+                 ->setPassword($passwordEncoder->encodePassword(
+                    $user,
+                    $registerForm['password']->getData()
+                 ))
+                 ->setIsConfirmed(false)
+            ;
+
+            $em->persist($user);
+            $em->flush();
+
+            $this->addFlash('success', 'Vous etes inscrits!');
+
+        }
+
+
+        return $this->render('security/register.html.twig', [
+            'register_form'=> $registerForm->createView()
+            
+        ]);       
+    }
+
+
+
+
+
     /**
      * @Route("/logout", name="app_logout")
      */
@@ -33,4 +78,13 @@ class SecurityController extends AbstractController
     {
         throw new \Exception('This method can be blank - it will be intercepted by the logout key on your firewall');
     }
+
+
+     
+
+
+
+
+
+
 }
---
+++ src/Form/UserRegistrationFormType.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Regex;
+
+class UserRegistrationFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('email', EmailType::class, [
+                'constraints'=> [
+                    new NotBlank(['message' => 'Veuillez entrer un email.']),
+                    new Email(['message' => 'Veuillez rentrer une adresse valide.'])
+                ]
+            ])
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'mapped'=>false,
+                'required' => true,
+                'invalid_message' => 'Les mots de passe ne correspondent pas',
+                'first_options' => ['label'=>' Votre mot de passe'],
+                'second_options'=> ['label'=>'Confirmation de votre mot de passe'],
+                'constraints' => [
+                    new NotBlank(['message' => 'Veuillez indiquer un mot de passe.']),
+                    new Regex([
+                        'pattern' => '/^[a-z0-9-_]+$/i',
+                        'message' => 'Le mot de passe ne peut contenir que des caractères alphanumériques.'
+                    ]),
+                    new Length([
+                        'min' => 5,
+                        'minMessage' => 'Le mot de passe doit contenir au moins 5 caractères'
+                    ])
+                ]
+                ])
+            ->add('pseudo', TextType::class, [
+                'constraints' => [
+                    new NotBlank(['message' => 'Veuillez indiquer un pseudo.']),
+                    new Regex([
+                        'pattern' => '/^[a-z0-9-_]+$/i',
+                        'message' => 'Le pseudo ne peut contenir que des caractères alphanumériques.'
+                    ]),
+                    new Length([
+                        'min' => 3,
+                        'minMessage' => 'Le pseudo doit contenir au moins 3 caractères',
+                        'max' => 40,
+                        'maxMessage' => 'Le pseudo ne peut contenir plus de 40 caractères',
+                    ])
+                ]
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
